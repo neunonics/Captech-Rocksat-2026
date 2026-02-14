e2c6197059be5583b2c29a807e9048932475a6bb
neunonics <66859264+neunonics@users.noreply.github.com>
2026-02-14 01:03:33 -0500

    Add files via upload

--- FSW/src/data.cpp
+++ FSW/src/data.cpp
@@ -9,8 +9,9 @@ struct FSW_DATETIME{
     u_int8_t hour;
     u_int8_t second;
     u_int8_t month;
-    u_int8_t miniute;
+    u_int8_t minute;
     u_int8_t year;
+    uint32_t subseconds;
 };
 
 struct FSW_GAMMA_DATA {
@@ -28,10 +29,70 @@ struct FSW_STATUS{
     bool led_sd;
     bool led_hrtbt;
     int error_code;
+    bool is_armed;
+    bool is_te2;
+
+    bool spect_1_connected;
+    bool spect_2_connected;
+    bool orin_connected;
+    bool comms_connected;
+    bool bno1_connected;
+    bool bno2_connected;
 
 };
 
-FSW_GAMMA_DATA fetchSpectrum(int spect, HardwareSerial *uart) {
+struct FSW_IMU_EULER{
+    float roll;
+    float pitch;
+    float yaw;
+};
+
+struct FSW_IMU_XYZ{
+    float x;
+    float y;
+    float z;
+};
+
+struct FSW_IMU_QUAT{
+    float q1;
+    float q2;
+    float q3;
+    float q4;
+};
+
+struct FSW_IMU{
+    FSW_IMU_EULER euler;
+    FSW_IMU_XYZ bfield;
+    FSW_IMU_XYZ gravity;
+    FSW_IMU_QUAT quaternion;
+};
+
+struct FSW_ADS{
+    FSW_IMU imu1;
+    FSW_IMU imu2;
+};
+
+struct FSW_PREDICTION{
+    int priority;
+    int spectrumID;
+    String prediction;
+    float probability;
+};
+
+struct FSW_POWER{
+    float voltage;
+    float current;
+    float power;
+};
+
+struct FSW_EDS{
+    FSW_POWER twelveV;
+    FSW_POWER fiveV;
+    FSW_POWER threeV;
+
+};
+
+FSW_GAMMA_DATA fetchSpectrum(int spect, HardwareSerial *uart, STM32RTC *rtc) {
     FSW_GAMMA_DATA data;
     data.spectrumID = spect;
     data.valid = false;
@@ -43,6 +104,8 @@ FSW_GAMMA_DATA fetchSpectrum(int spect, HardwareSerial *uart) {
 
     // 1. Clear any junk in the serial buffer before sending command
     while(uart->available()) uart->read();
+
+    uart->setTimeout(2000);
 
     // 2. Send the specific command from the help menu
     uart->println("read spectrum"); 
@@ -76,8 +139,15 @@ FSW_GAMMA_DATA fetchSpectrum(int spect, HardwareSerial *uart) {
     // 5. Final check
     if (binIndex > 0) {
         data.valid = true;
-        //data.timestamp = millis();
+        data.datetime.second = rtc->getSeconds();
+        data.datetime.minute = rtc->getMinutes();
+        data.datetime.hour   = rtc->getHours();
+        data.datetime.day     = rtc->getDay();
+        data.datetime.month   = rtc->getMonth();
+        data.datetime.year    = rtc->getYear();
     }
+
+    uart->setTimeout(1000);
 
     return data;
 }
